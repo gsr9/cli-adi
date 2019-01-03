cb2d5c9968eba0d37b79df44791910b044141f9e
Guillermo Serra Ruidíaz <gsr9@alu.ua.es>
2019-01-03 12:40:39 +0100

    Añadida la barra de navegación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+//Material design
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {
+  MatButtonModule,
+  MatInputModule,
+  MatIconModule,
+  MatSelectModule,
+  MatToolbarModule
+} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -10,7 +30,20 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //Material design
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatListModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
