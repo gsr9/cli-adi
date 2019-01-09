52c061b8f4b301a788781e3962cf53882fac411a
Guillermo Serra Ruidíaz <gsr9@alu.ua.es>
2019-01-09 10:54:03 +0100

    Login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router
+        // private authenticationService: AuthenticationService,
+        // private alertService: AlertService
+      ) { }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    console.log(this.f.email.value, this.f.password.value)
+  }
+
+
+}
